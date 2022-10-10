e11a8428381468756fafa76fd94b56d3be1426c4
lixinglin <lixinglin@frontis.cn>
2022-10-10 16:05:15 +0800

    Initial commit

---
+++ src/main/java/com/example/girlspringbootstarter/GirlSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.girlspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlSpringbootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GirlSpringbootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/girlspringbootstarter/GirlSpringbootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.girlspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlSpringbootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
